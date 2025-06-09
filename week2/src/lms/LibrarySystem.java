75c1e8c74d15865ac0834ef5c3bfc6e4d68b0111
Assylzhan-Izbassar <Assylzhan-Izbassar>
2025-06-09 15:16:00 +0500

    upd

--- week2/src/lms/LibrarySystem.java
+++ week2/src/lms/LibrarySystem.java
@@ -1,6 +1,7 @@
 package lms;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class LibrarySystem {
 	public static void main(String[] args) {
@@ -10,29 +11,51 @@ public class LibrarySystem {
 		boolean run = true; 
 		System.out.println("Welcome to the LMS!");
 		System.out.println("Please, write down the options");
-		System.out.println("create user");
 		System.out.println("create book");
+		System.out.println("display books");
+		System.out.println("assign book");
 		System.out.println("quit");
 		
 		while(run) {
 			System.out.println("Enter the query: ");	
 			String query = s.nextLine();
 			
-			if (query.equals("create user")) {
-				// user
-			} else if (query.equals("create book")) {
+			if (query.equals("create book")) {
 				String bookName = s.nextLine();
 				Book b = new Book(bookName);
 				
 				lib.addBook(b);
 				
 				System.out.println("Book is added");
+			} else if (query.equals("display books")) {
+				Vector<Book> currBooks = lib.getBooks();
+				for (int i = 0; i < currBooks.size(); ++i) {
+					Book currBook = currBooks.elementAt(i);
+					System.out.println(currBook.getName() 
+							+ " " + currBook.getUser());
+				}
 			} else if (query.equals("assign book")) {
+				System.out.println("Please, enter the username: ");
 				String username = s.nextLine();
-				// show books
-				// enter ID of book / name
+				Vector<Book> currBooks = lib.getBooks();
 				
-//				lib.assign(b, new User(username));
+				for (int i = 0; i < currBooks.size(); ++i) {
+					System.out.println(currBooks.elementAt(i));
+				}
+				
+				System.out.println("Please, write down the book name: ");
+				String bookName = s.nextLine();
+				
+				Book selected = null;
+				for (int i = 0; i < currBooks.size(); ++i) {
+					if(currBooks.elementAt(i).getName().equals(bookName)) {
+						selected = currBooks.elementAt(i);
+					}
+				}
+				
+				lib.assign(selected, new User(username));
+				
+				System.out.println("The book is assigned!");
 			}
 			else if (query.equals("quit")) {
 				run = false;
--- week2/src/lms/User.java
+++ week2/src/lms/User.java
@@ -3,6 +3,19 @@ package lms;
 import java.util.Vector;
 
 public class User {
+	private String name;
 	Vector<Book> books;
-
+	
+	{
+		this.books = new Vector<Book>();
+	}
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return this.name;
+	}
+	
 }
